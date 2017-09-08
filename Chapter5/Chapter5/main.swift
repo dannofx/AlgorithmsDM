4e8b0612837e36956eba94f5765b6c1dcad3ca33
Daniel Heredia <danno@mistercyb.org>
2017-09-07 19:20:05 -0500

    Chapter 5 challenges added (2 and 4)

---
+++ Chapter5/Chapter5/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Chapter5
+//
+//  Created by Danno on 9/6/17.
+//  Copyright © 2017 Daniel Heredia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
