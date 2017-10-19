c826daf20daf009085e0ce6579ecab889893f31d
Daniel Heredia <danno@mistercyb.org>
2017-10-19 00:41:08 -0500

    KDTree and line structure added

---
+++ Others/KDTree.swift
@@ -0,0 +1,89 @@
+import Foundation
+
+public class KDTree: NSObject {
+    let dimensionsSize: Int
+    let dimensionIndex: Int
+    let value: [Float]
+    var right: KDTree?
+    var left: KDTree?
+    public init(value: [Float], dimensionIndex: Int) {
+        self.value = value
+        self.dimensionsSize = value.count
+        self.dimensionIndex = dimensionIndex
+    }
+    
+    var isLeaf: Bool {
+        return self.right == nil && self.left == nil
+    }
+    
+    func distance(to point: [Float]) -> Float {
+        return sqrt(zip(self.value, point).map{ pow(($0 - $1), 2) }.reduce(0.0) { $0 + $1 })
+    }
+    
+    func getSubTree(forPoint point: [Float]) -> KDTree? {
+        return point[self.dimensionIndex] > self.value[dimensionIndex] ? self.right : self.left
+    }
+    func getComplementSubTree(forPoint point: [Float]) -> KDTree? {
+        return point[self.dimensionIndex] <= self.value[dimensionIndex] ? self.right : self.left
+    }
+    
+    func dimensionDistance(forPoint point: [Float]) -> Float {
+        return abs(point[self.dimensionIndex] - self.value[dimensionIndex])
+    }
+    
+}
+
+func makeTree(fromPoints points:[[Float]], dimensionSize: Int, dimensionIndex: Int = 0) -> KDTree?{
+    if points.count == 0 {
+        return nil
+    }
+    let sortedPoints = points.sorted { $0[dimensionIndex] < $1[dimensionIndex] }
+    let medianIndex = Int(sortedPoints.count / 2)
+    let median = sortedPoints[medianIndex]
+    let tree = KDTree.init(value: median, dimensionIndex: dimensionIndex)
+    if sortedPoints.count <= 1 {
+        return tree
+    }
+    let newDimensionIndex = (dimensionIndex + 1) % dimensionSize
+    tree.left = makeTree(fromPoints: Array(sortedPoints[0..<medianIndex]), dimensionSize: dimensionSize, dimensionIndex: newDimensionIndex)
+    tree.right = makeTree(fromPoints: Array(sortedPoints[(medianIndex + 1)..<sortedPoints.count]), dimensionSize: dimensionSize, dimensionIndex: newDimensionIndex)
+    return tree
+}
+
+func findNearestNeighbor(tree: KDTree, point: [Float], minDistance: Float = Float.infinity, nearestNeighbor: KDTree? = nil) -> (nearest: KDTree, distance: Float) {
+    let distance = tree.distance(to: point)
+    let currentMinDistance: Float!
+    let currentNearestNeighbor: KDTree!
+    if distance < minDistance {
+        currentMinDistance = distance
+        currentNearestNeighbor = tree
+    } else {
+        currentMinDistance = minDistance
+        currentNearestNeighbor = nearestNeighbor
+    }
+    
+    if let subtree = tree.getSubTree(forPoint: point) {
+        let newResults = findNearestNeighbor(tree: subtree, point: point, minDistance: currentMinDistance, nearestNeighbor: currentNearestNeighbor)
+        let dimensionDistance = tree.dimensionDistance(forPoint: point)
+        if newResults.distance > dimensionDistance {
+            if let complementSubtree = tree.getComplementSubTree(forPoint: point) {
+                return findNearestNeighbor(tree: complementSubtree, point: point, minDistance: newResults.distance, nearestNeighbor: newResults.nearest)
+            }
+        }
+        return newResults
+    } else {
+        return (currentNearestNeighbor, currentMinDistance)
+    }
+}
+
+let dimensions = 2
+let points: [[Float]] = [[-0.5, 0],
+                          [-0.1, -1],
+                          [0.0, -1.0],
+                          [0.1, 0.5],
+                          [1.0, 0.0]]
+let targetPoint: [Float] = [-0.1, 0.5]
+let tree = makeTree(fromPoints: points, dimensionSize: dimensions)!
+let result = findNearestNeighbor(tree: tree, point: targetPoint)
+print("Neaest point \(targetPoint): \(result.nearest.value) distance \(result.distance)")
+
---
+++ Others/Line.swift
@@ -0,0 +1,76 @@
+import Foundation
+
+let epsilon = 0.00001
+
+struct Point {
+    var x: Double
+    var y: Double
+}
+
+struct Line {
+    var a: Double // X coefficcient
+    var b: Double // Y coefficcient
+    var c: Double // Constant term
+    
+    init(point1: Point, point2: Point) {
+        if point1.x == point2.x {
+            self.a = 1
+            self.b = 0
+            self.c = -point1.x
+        } else {
+            self.b = 1
+            self.a = -(point1.y - point2.y) / (point1.x - point2.x)
+            self.c = -(self.a * point1.x) - (self.b * point1.x)
+        }
+    }
+    
+    init(slope: Double, point: Point) {
+        self.a = -slope
+        self.b = 1
+        self.c = -(self.a * point.x) - (self.b * point.x)
+    }
+    
+    func isParallel(line: Line) -> Bool {
+        return abs(self.a - line.a) <= epsilon && abs(self.b - line.b) <= epsilon
+    }
+    
+    func isSameLine(_ line: Line) -> Bool {
+        return self.isParallel(line: line) && abs(self.c - line.c) <= epsilon
+    }
+    
+    func findIntersection(line: Line) -> Point? {
+        if self.isSameLine(line) {
+            print("Warning: Identical lines, all points intersect.")
+            return Point(x: 0.0, y: 0.0)
+        }
+        if self.isParallel(line: line) {
+            print("Error: Distinct parallel lines do not intersect.")
+            return nil
+        }
+        let x: Double = (line.b * self.c - self.b - line.c) / (line.a * self.b - self.a * line.b)
+        var y: Double!
+        if abs(self.b) > epsilon {
+            y = -(self.a * x + self.c) / self.b
+        } else {
+            y = -(line.a * x + line.c) / line.b
+        }
+        return Point(x: x, y: y)
+    }
+    
+    func findClosestPoint(toPoint point: Point) -> Point {
+        var result = Point(x:0.0, y:0.0)
+        if abs(self.b) <= epsilon {
+            result.x = -self.c
+            result.y = point.y
+        } else if abs(self.a) <= epsilon {
+            result.y = -self.c
+            result.x = point.x
+        } else {
+            let line = Line(slope: (1 / self.a), point: point)
+            result = self.findIntersection(line: line)!
+        }
+        return result
+    }
+}
+
+
