9d6bc33a442f13137704b8b3ce47f6cf8bfc6499
Daniel Heredia <danno@mistercyb.org>
2017-11-08 00:25:02 -0600

    Some work to delete elements in a BTree (still uncomplete)

--- Others/BTree.swift
+++ Others/BTree.swift
@@ -77,6 +77,35 @@ class BTreeNode<Element> where Element: Comparable {
         }
     }
     
+    func remove(_ item: Element) {
+        let removeIndex = self.findKey(item)
+        if removeIndex < self.keys.count && self.keys[removeIndex] == item {
+            // The key to remove is present in this node
+            if self.leaf {
+                self.removeFromLeaf(keyIndex: removeIndex)
+            } else {
+                self.removeFromNonLeaf(childIndex: removeIndex)
+            }
+        } else {
+            if self.leaf {
+                print("The item can't be removed because it doesn't exist in the tree")
+                return
+            }
+            let lastNode = (removeIndex == self.keys.count) // Is in the last node?
+            // If the child has than 'degree' keys, then fill that child
+            if self.children[removeIndex].keys.count < self.degree {
+                self.fill(removeIndex)
+            }
+            if lastNode && removeIndex > self.keys.count {
+                // Child has been merged with the previous child
+                self.children[removeIndex - 1].remove(item)
+            } else {
+                // Remove item from the expected child
+                self.children[removeIndex].remove(item)
+            }
+        }
+    }
+    
     func splitChild(_ i: Int, oldNode: BTreeNode) {
         let newNode = BTreeNode(degree: oldNode.degree, leaf: oldNode.leaf)
         for j in 0..<(self.degree - 1) {
@@ -93,6 +122,74 @@ class BTreeNode<Element> where Element: Comparable {
         oldNode.keys.removeLast()
         self.keys.insert(newKey, at: i)
         self.children.insert(newNode, at: (i + 1))
+    }
+}
+
+// MARK: Delete utilities for BTreeNode
+
+private extension BTreeNode {
+    
+    func findKey(_ k: Element) -> Int{
+        for i in 0..<self.keys.count {
+            if self.keys[i] >= k {
+                return i
+            }
+        }
+        return self.keys.count
+    }
+    
+    func removeFromLeaf(keyIndex removeIndex: Int) {
+        self.keys.remove(at: removeIndex)
+    }
+    
+    func removeFromNonLeaf(childIndex removeIndex: Int) {
+        if self.children[removeIndex].keys.count >= self.degree {
+            // If the child that precedes the item to remove has at least
+            // 'degree' keys, find the predecesor of the item (key) and
+            // replace the item to remove by this, then delete the original
+            // predecesor item.
+            let predecesorItem = self.getPredecesor(keyIndex: removeIndex)
+            self.keys[removeIndex] = predecesorItem
+            self.children[removeIndex].remove(predecesorItem)
+        } else if (self.children[removeIndex + 1].keys.count >= self.degree) {
+            // If the child that success the item to remove has at least
+            // 'degree' keys, find the successor of the item (key) and
+            // replace the item to remove by this, then delete the original
+            // successor item.
+            let successorItem = self.getSuccessor(keyIndex: removeIndex)
+            self.keys[removeIndex] = successorItem
+            self.children[removeIndex + 1].remove(successorItem)
+        } else {
+            let item = self.keys[removeIndex]
+            self.mergeWithNextChild(childIndex: removeIndex)
+            self.children[removeIndex].remove(item)
+        }
+    }
+    
+    func getPredecesor(keyIndex: Int) -> Element {
+        //TODO: Implementation
+        return 1 as! Element
+    }
+    
+    func getSuccessor(keyIndex: Int) -> Element {
+        //TODO: Implementation
+        return 1 as! Element
+    }
+    
+    func fill(_ index: Int) {
+        //TODO: Implementation
+    }
+    
+    func borrowFromNextChild(childIndex: Int) {
+        //TODO: Implementation
+    }
+    
+    func borrowFromPreviousChild(childIndex: Int) {
+        //TODO: Implementation
+    }
+    
+    func mergeWithNextChild(childIndex: Int) {
+        //TODO: Implementation
     }
 }
 
@@ -138,6 +235,21 @@ class BTree<Element> where Element: Comparable {
             let root = BTreeNode<Element>(degree: self.degree, leaf: true)
             root.keys.append(item)
             self.root = root
+        }
+    }
+    
+    func remove(_ item: Element) {
+        if let root = self.root {
+            root.remove(item)
+            if root.keys.count == 0 {
+                if root.leaf {
+                    self.root = nil
+                } else {
+                    self.root = root.children.first!
+                }
+            }
+        } else {
+            print("The item was not removed because the tree is empty")
         }
     }
 }
