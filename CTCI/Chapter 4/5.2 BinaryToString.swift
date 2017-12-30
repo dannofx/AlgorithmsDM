0b2d799e2961f33d58037016a94ffc43b7c0c7e3
Daniel Heredia <danno@mistercyb.org>
2017-12-29 22:28:28 -0500

    Bit manipulation exercise added

---
+++ CTCI/Chapter 4/5.2 BinaryToString.swift
@@ -0,0 +1,47 @@
+// Binary to String
+
+import Foundation
+
+infix operator >>>> : BitwiseShiftPrecedence
+
+extension Int {
+
+    var binaryString: String {
+        let mask = 0b1 << (Int.bitWidth - 1)
+        let signBit = ( mask & self ) != 0 ? "1" : "0"
+        let number = ~mask & self
+        let binaryForm = String(number, radix: 2)
+        return signBit + String(repeatElement("0", count: Int.bitWidth - binaryForm.count - 1)) + binaryForm
+    }
+
+    static func >>>> (lhs: Int, rhs: Int) -> Int {
+        let mask = ~( ~0 << (Int.bitWidth - rhs))
+        return ( lhs >> rhs ) & mask
+    }
+}
+
+func printFraction(number: Double) {
+    if number <= 0.0 || number >= 1.0 {
+        print("Error: Number is not a fraction.")
+        return
+    }
+    var remaining =  number
+    var result = 0
+    var size = 0
+    while remaining > 0 {
+        if size == 32 {
+            print("Error: The number will not be represented accurately. \(String(result, radix: 2))")
+            return
+        }
+        size += 1
+        remaining = remaining * 2
+        result = result << 1 | Int(remaining)
+        if remaining >= 1.0 {
+            remaining -= 1.0
+        }
+    }
+    print("." + String(result, radix: 2))
+}
+
+
+printFraction(number: 0.375)
