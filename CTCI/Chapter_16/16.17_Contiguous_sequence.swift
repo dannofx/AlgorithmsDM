02f1d3fb46a927941022fc295e0a454e7fecb841
Daniel Heredia <danno@mistercyb.org>
2018-04-26 00:16:46 -0500

    Optimized solution for 16.17 (It's not necessary copy the subarray each time

--- CTCI/Chapter_16/16.17_Contiguous_sequence.swift
+++ CTCI/Chapter_16/16.17_Contiguous_sequence.swift
@@ -8,22 +8,22 @@
 import Foundation
 
 func findMaxSubSequence(_ sequence: [Int]) -> (sum: Int, subsequence: [Int]) {
-    var currentSub = [Int]()
-    var currentSum = 0
-    var maxSub = [Int]()
-    var maxSum = 0
-    for i in 0..<sequence.count {
-        currentSub.append(sequence[i])
-        currentSum += sequence[i]
-        if currentSum > maxSum {
-            maxSum = currentSum
-            maxSub = currentSub
-        } else if currentSum < 0 {
-            currentSub = [Int]()
-            currentSum = 0
-        }
+  var sub = (start: 0, end: 0)
+  var sum = 0
+  var maxSub = (start: 0, end: 0)
+  var maxSum = 0
+  for (i, number) in sequence.enumerated() {
+    sum += number
+    sub.end = i
+    if sum > maxSum {
+      maxSum = sum
+      maxSub = sub
+    } else if sum < 0 {
+      sum = 0
+      sub = (start: i + 1, end: i + 1)
     }
-    return (maxSum, maxSub)
+  }
+  return (maxSum, [Int](sequence[maxSub.start...maxSub.end]))
 }
 
 let sequence = [2, -8, 3, -2, 4, -10]
@@ -33,4 +33,3 @@ let max = findMaxSubSequence(sequence)
 print("Sequence: \(sequence)")
 print("Max sum: \(max.sum)")
 print("Max subsequence: \(max.subsequence)")
-
